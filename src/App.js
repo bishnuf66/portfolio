6105e2ca4da5c8fc8378ce420d7eca079ce16133
bishnuf66 <bkbishnu2000@gmail.com>
2024-06-18 15:42:57 +0545

    fixed css

--- src/App.js
+++ src/App.js
@@ -15,21 +15,16 @@ function App() {
 
   return (
     <Router>
-
-      <div className={` container transition-all duration-700 ease-in-out w-full min-h-screen ${isDarkMode ? 'bg-black' : 'bg-gray-50'}`}>
-
+      <div className={`min-h-screen w-full ${isDarkMode ? 'bg-black' : 'bg-gray-50'}`}>
         <Design isDarkMode={isDarkMode} />
-
-
-        <div className={` w-full top-0 p-0  sm:px-10 sm:py-4 ${isDarkMode ? 'text-white' : 'text-black'} `}>
+        <div className={`w-full p-0 sm:px-10 sm:py-4 ${isDarkMode ? 'text-white' : 'text-black'}`}>
           <Header isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode} />
         </div>
-
-        <div className="flex flex-row px-0 py-2  sm:px-10">
+        <div className="flex flex-row px-0 py-2 sm:px-10 min-h-screen">
           <div className={`w-1/4 p-0 sm:py-4 sm:p-4 ${isDarkMode ? 'text-white' : 'text-black'}`}>
             <Sidebar isDarkMode={isDarkMode} />
           </div>
-          <div className="flex justify-center items-center p-4">
+          <div className="flex-grow flex justify-start items-start p-4">
             <Routes>
               <Route path="/" element={<Home isDarkMode={isDarkMode} />} />
               <Route path="/projects" element={<Projects isDarkMode={isDarkMode} />} />
@@ -38,10 +33,6 @@ function App() {
           </div>
         </div>
       </div>
-      <div>
-
-      </div>
-
     </Router>
   );
 }
